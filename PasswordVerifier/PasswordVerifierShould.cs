7e074d9592e764979d767e77ae9fd1eda133a912
Miguel Angel Martin Hrdez <miguel.martin@analyticalways.com>
2017-10-04 12:26:49 +0200

    PasswordVerifier password_must_have_at_least_8_characters

---
+++ PasswordVerifier/PasswordVerifierShould.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PasswordVerifier
+{
+    public class PasswordVerifierShould
+    {
+        [Fact]
+        public void password_must_have_at_least_8_characters()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            bool result = passwordVerifier.Verify("pass");
+            result.Should().Be(false);
+        }
+    }
+
+    public class PasswordVerifier
+    {
+        public bool Verify(string password)
+        {
+            return password.Length == 8;
+        }
+    }
+}
